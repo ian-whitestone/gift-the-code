d97aa9f025c94ec86e4e3e3bb14c6582a2ef41ac
iwhitest <ianjameswhitestone@gmail.com>
2016-10-22 09:22:43 -0400

    get all location data

--- data_import.py
+++ data_import.py
@@ -2,6 +2,7 @@ import xlrd
 import xlsxwriter
 import os
 import datetime
+import pandas as pd
 
 
 def read_data(src):
@@ -56,18 +57,48 @@ def parse_data(data):
         data3.append(tup)
     return data3
 
+def parse_data_mod(data):
+    ints=[0,1,2,3,4,5,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]
+    data2=[tuple([d[i] for i in ints]) for d in data]
+    data3=[]
+    for j,data in enumerate(data2[1:]):
+        entry=[]
+        for i,d in enumerate(data):
+            if i==0:
+                entry.append(parse_date(d))
+            elif i==3:
+                ss=d.split('|')
+                if len(ss)>1:
+                    entry.append(ss[0].strip())
+                    entry.append(ss[1].strip())
+                else:
+                    entry.append(ss[0].strip())
+                    entry.append(None)
+            elif i in [6,7]:
+                entry.append(parse_timestamp(d))
+            else:
+                if isinstance(d,str):
+                    entry.append(d.strip())
+                else:
+                    entry.append(d)
+        data3.append(entry)
+    return data3
+
 
 def main():
     src_files = [f for f in os.listdir('data') if f[-1] in 'Xx']
-
     for src in src_files:
         data=read_data(src)
-        parsed_data=parse_data(data)
+        parsed_data=parse_data_mod(data)
         break
     return parsed_data
 
-# main()
-
+# data=main()
+#
+# headers=['date', 'trans_type', 'route_desc', 'stop_type','source_type','driver', 'donor_id', 'arrive', 'depart', 'bread', 'baked', 'dairy', 'produce', 'protein', 'prepared', 'bev_juice', 'bev_other', 'snack', 'non_perish', 'non_food', 'quality', 'zero_lbs', 'postcode']
+# # headers=data.pop(0)
+# df=pd.DataFrame(data, columns=headers)
+# print(df.head())
 ###TO DO
 ##raise exception if columns are not matching expected type
 ##add instructions (i.e. info must be in sheet 0)
--- google.py
+++ google.py
@@ -1,8 +1,8 @@
-import request
+import requests
 
 
 req_url='https://maps.googleapis.com/maps/api/geocode/json?address={0}&key={1}'
-api_key = Ugen.ConfigSectionMap('googlemaps')['key']
+api_key = 'AIzaSyBrfDTcZ9m3FnRBN4OorWa96Ox0Xa7IHfQ'
 
 def get_coords(addr):
     address=addr +' Toronto, Ontario, Canada'
@@ -11,9 +11,24 @@ def get_coords(addr):
         response_dict = response.json()
         coords=response_dict['results'][0]['geometry']['location']
         if response_dict['status'] == 'OK':
-        #             print (response_dict)
+            print (response_dict)
             return coords['lat'],coords['lng'],response_dict['results'][0]['formatted_address'].replace(',','')
     except Exception as err:
         print (str(err))
         pass
     return None,None,None
+
+def get_location_data(addr):
+    address=addr +' Toronto, Ontario, Canada'
+    try:
+        response = requests.get(req_url.format(address,api_key))
+        response_dict = response.json()
+        coords=response_dict['results'][0]['geometry']['location']
+        if response_dict['status'] == 'OK':
+            return response_dict['results']
+    except Exception as err:
+        print (str(err))
+        pass
+    return None
+
+# get_coords('M4Y 1N2')
