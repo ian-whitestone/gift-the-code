d2c873bdfe4e75fe1a6808e336f62159d1c80337
iwhitest <ianjameswhitestone@gmail.com>
2016-10-22 13:37:00 -0400

    data prep for d3 viz

---
+++ map_data.py
@@ -0,0 +1,29 @@
+import database_operations as dbo
+import pandas as pd
+import numpy as np
+
+
+def perishable_ratio(df):
+    if df['total']==0:
+        return 0
+    else:
+        return (df['non_food']+df['non_perish'])/df['total']
+
+def nutrient_ratio(df):
+    if df['total']==0:
+        return 0
+    else:
+        return (df['protein']+df['produce']+df['dairy'])/df['total']
+
+def get_map_data():
+    ##query db
+
+    ##
+    df['year']=df['date'].map(lambda x: int(str(x)[0:4]))
+    sum_col_list=['bread', 'baked', 'dairy','produce', 'protein', 'prepared', 'bev_juice', 'bev_other', 'snack','non_perish', 'non_food']
+    df['total']=df[sum_col_list].sum(axis=1)
+    df['perishable_ratio']=df.apply(perishable_ratio,axis=1)
+    df['nutrient_ratio']=df.apply(nutrient_ratio,axis=1)
+    grouped_df=df.groupby(['stop_type','year','postcode']).agg({'total':np.sum,'nutrient_ratio':np.mean,'perishable_ratio':np.mean}).reset_index()
+
+    return
