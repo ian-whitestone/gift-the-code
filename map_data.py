548f776b461ed2399da087444bda6113cb113311
iwhitest <ianjameswhitestone@gmail.com>
2016-10-22 14:32:51 -0400

    data viz init files

--- map_data.py
+++ map_data.py
@@ -18,12 +18,17 @@ def nutrient_ratio(df):
 def get_map_data():
     ##query db
 
-    ##
+    ##filter out rows missing postal codes
+    df=df[df['postcode'].notnull()]
+
+    #create addl fields
     df['year']=df['date'].map(lambda x: int(str(x)[0:4]))
     sum_col_list=['bread', 'baked', 'dairy','produce', 'protein', 'prepared', 'bev_juice', 'bev_other', 'snack','non_perish', 'non_food']
     df['total']=df[sum_col_list].sum(axis=1)
     df['perishable_ratio']=df.apply(perishable_ratio,axis=1)
     df['nutrient_ratio']=df.apply(nutrient_ratio,axis=1)
-    grouped_df=df.groupby(['stop_type','year','postcode']).agg({'total':np.sum,'nutrient_ratio':np.mean,'perishable_ratio':np.mean}).reset_index()
+
+    #get summary df
+    grouped_df=df.groupby(['stop_type','year','postcode','long','lat','neighbourhood','locality']).agg({'total':np.sum,'nutrient_ratio':np.mean,'perishable_ratio':np.mean}).reset_index()
 
     return
