440c0c348e193af67f85cf3ca512c4f604ae1dd1
dlin17 <darrenlin17@gmail.com>
2016-10-27 22:29:11 +0000

    fixed refresh

--- flask/web/__init__.py
+++ flask/web/__init__.py
@@ -51,20 +51,20 @@ def make_dicts(cursor, row):
 ###########################################################################
 
 # Jinja extensions and filters
-app.jinja_env.add_extension('jinja2.ext.do')
-app.jinja_env.add_extension('jinja2.ext.loopcontrols')
+# app.jinja_env.add_extension('jinja2.ext.do')
+# app.jinja_env.add_extension('jinja2.ext.loopcontrols')
 
-_paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
+# _paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
 
 
-@app.template_filter()
-@evalcontextfilter
-def nl2br(eval_ctx, value):
-    result = u'\n\n'.join(u'<p>%s</p>' % p.replace('\n', '<br>\n')
-                          for p in _paragraph_re.split(escape(value)))
-    if eval_ctx.autoescape:
-        result = Markup(result)
-    return result
+# @app.template_filter()
+# @evalcontextfilter
+# def nl2br(eval_ctx, value):
+#     result = u'\n\n'.join(u'<p>%s</p>' % p.replace('\n', '<br>\n')
+#                           for p in _paragraph_re.split(escape(value)))
+#     if eval_ctx.autoescape:
+#         result = Markup(result)
+#     return result
 
 
 # @app.before_request
